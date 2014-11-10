fe14a408a0f3de321596683fccb1297f5da17189
jamesascarter <jamesascarter@gmail.com>
2014-11-10 13:54:18 +0000

    can post photos and create a caption

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,24 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to '/photos'
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+
+  private
+
+  def photo_params
+    params[:photo].permit(:caption, :image)
+  end
 end
---
+++ spec/features/photos_feature_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe 'photos' do
+  context 'no photos have yet been added' do
+
+    it 'should notify user when no photos on feed' do
+      visit '/photos'
+      expect(page).to have_content 'No photos'
+    end
+
+    it 'should allow user to click upload link' do
+      visit '/photos'
+      expect(page).to have_link 'Add a photo'
+    end
+
+  end
+
+  context 'have been uploaded' do
+    before do
+      Photo.create(caption:'lovely stuff', image: File.open("#{Rails.root}/spec/fixtures/lager.jpeg"))
+    end
+
+    it 'user can see photo' do
+      visit '/photos'
+      expect(page).to have_css 'img'
+    end
+
+    it 'user can see caption' do
+      visit '/photos'
+      expect(page).to have_content 'lovely stuff'
+    end
+  end
+end
+
+describe 'adding new photos' do
+
+    it 'prompts user to use uploader and will display the caption on the page' do
+      visit '/photos'
+      click_link 'Add a photo'
+      fill_in 'Caption', with: 'lovely stuff'
+      click_button 'Upload photo'
+      expect(page).to have_content 'lovely stuff'
+      expect(current_path).to eq '/photos'
+    end
+
+    it 'allows user to upload a photo and view it on the page' do
+      visit '/photos'
+      click_link 'Add a photo'
+      fill_in 'Caption', with: 'lovely stuff'
+      attach_file 'Image', "public/owl.jpeg"
+      click_button 'Upload photo'
+      expect(page).to have_css 'img'
+    end
+end
+
+describe 'viewing photos' do
+    before do
+      @owly = Photo.create(caption:'lovely stuff', image: File.open("#{Rails.root}/spec/fixtures/lager.jpeg"))
+    end
+
+    it 'lets a user view a photo' do
+      visit '/photos'
+      click_link 'lovely stuff'
+      expect(current_path).to eq "/photos/#{@owly.id}"
+    end
+end
+
+
